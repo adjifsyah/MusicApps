918ef129c67a257f29072a53e4fe7266c8f80a21
Adji Firmansyah <adji.firmansyah27@icloud.com>
2022-05-28 01:50:15 +0700

    add project

---
+++ MusicApp/MusicApp/MusicViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MusicViewController.swift
+//  MusicApp
+//
+//  Created by Adji Firmansyah on 5/28/22.
+//
+
+import UIKit
+
+class MusicViewController: UIViewController {
+
+    var heightConstraint: NSLayoutConstraint?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
